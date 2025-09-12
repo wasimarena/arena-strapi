67692ef2f06de2f8aeed3710aa5f27a389bf0389
eric-arenatwo <cto@arenatwo.com>
2025-09-12 10:37:55 +0300

    Update plugins.ts with sendgrid

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'no-reply@arenatwo.com',
+        defaultReplyTo: 'no-reply@arenatwo.com',
+        testAddress: 'cto@arenatwo.com',
+      },
+    },
+  },
+});
